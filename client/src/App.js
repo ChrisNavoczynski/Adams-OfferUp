13c75ed805b6fb4375440af959a8e46c94ee39d8
CourtneyHoppus <74206103+CourtneyHoppus@users.noreply.github.com>
2021-01-25 16:16:57 -0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -24,3 +24,21 @@ class App extends Component {
 }
 
 export default App;
+
+/* //Created by Chis Navoczynski
+//AD320- TeamAdams 
+
+import ItemPageHeader from './itemPage_components/item_header'
+import ItemPageSelection from './itemPage_components/selectItem'
+
+function App() {
+  return (
+    <div>
+        <ItemPageHeader/>
+        <ItemPageSelection/>
+
+    </div>
+  );
+}
+
+export default App; */
