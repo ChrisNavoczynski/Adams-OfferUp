44f71ba9305991bd021ca8f3dce413ad7a668f3c
CourtneyHoppus <74206103+CourtneyHoppus@users.noreply.github.com>
2021-02-02 22:01:56 -0800

    created users API

--- service/index.js
+++ service/index.js
@@ -4,16 +4,19 @@ const app = express();
 const port = 8000;
 
 const productsRouter = require('./routers/products');
-//const usersRouter = require('./routers/users');
+const usersRouter = require('./routers/users');
 
 app.use(express.json());
 
-//app.use('/v1/users', usersRouter);
+app.use('/v1/users', usersRouter);
 app.use('/v1/products', productsRouter);
 app.post('/v2/products', (req, res) => {
     res.send("Future Updates Go Here!")
-})
-
+});
+app.post('/v2/users', (req, res) => {
+    res.status(501);
+    res.send('New user update');
+});
 app.listen(port, () => {
     console.log(`My app is listening at http://localhost:${port}`);
 });
