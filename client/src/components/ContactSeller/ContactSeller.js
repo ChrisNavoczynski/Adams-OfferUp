7a4e713a0563937f4433f6f0013882a3658889d0
CNavo <chrisnavo01@gmail.com>
2021-02-22 17:36:02 -0800

    Updated w/Jason Eslint

--- client/src/components/ContactSeller/ContactSeller.js
+++ client/src/components/ContactSeller/ContactSeller.js
@@ -16,7 +16,7 @@ const ContactForm = () => {
       offer: offer.value,
       message: message.value,
     };
-    const response = await fetch('http://localhost:5000/contact', {
+    const response = await fetch('http://localhost:3000/contact', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
